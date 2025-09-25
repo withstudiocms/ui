1779190849e80ed21af4eeac3b36553cb4c8447f
John Memmott <122105474+RATIU5@users.noreply.github.com>
2025-09-25 10:48:47 -0600

    add: custom set ids on TabItem/Tabs for testing (#118)
    Co-authored-by: Louis Escher <66965600+louisescher@users.noreply.github.com>

---
+++ .changeset/famous-cycles-sell.md
@@ -0,0 +1,5 @@
+---
+"@studiocms/ui": minor 
+---
+
+Updates the Tabs components to optionally use a custom ID
--- packages/studiocms_ui/test/components/Tabs.test.ts
+++ packages/studiocms_ui/test/components/Tabs.test.ts
@@ -3,20 +3,20 @@ import TabItem from '../../src/components/Tabs/TabItem.astro';
 import Tabs from '../../src/components/Tabs/Tabs.astro';
 import { test } from '../fixtures/vitest/AstroContainer';
 
-// TODO Tab and TabItem have to much dynamic behavior to snapshot test effectively
-describe.todo('Tabs Component', () => {
-	test.todo('renders Tabs component correctly', async ({ renderComponent }) => {
+describe('Tabs Component', () => {
+	test('renders Tabs component correctly', async ({ renderComponent }) => {
 		const tabsContent = [];
 		for (let i = 1; i <= 3; i++) {
 			tabsContent.push(
 				await renderComponent(TabItem, 'TabItem', {
-					props: { label: `Tab ${i}` },
+					props: { label: `Tab ${i}`, id: `tab-${i}` },
 					slots: { default: `Content for Tab ${i}` },
 				})
 			);
 		}
 
 		const result = await renderComponent(Tabs, 'Tabs', {
+			props: { id: 'tab-container' },
 			slots: { default: tabsContent.join('') },
 		});
 		expect(result).toMatchSnapshot();
