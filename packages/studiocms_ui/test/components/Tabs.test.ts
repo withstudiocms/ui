361707d24d14989326b454f87145592bbaa88dde
RATIU5 <jcmemmott20@outlook.com>
2025-09-25 09:56:43 -0600

    fix: tabs test, removed todo tests

--- packages/studiocms_ui/test/components/Tabs.test.ts
+++ packages/studiocms_ui/test/components/Tabs.test.ts
@@ -3,9 +3,8 @@ import TabItem from '../../src/components/Tabs/TabItem.astro';
 import Tabs from '../../src/components/Tabs/Tabs.astro';
 import { test } from '../fixtures/vitest/AstroContainer';
 
-// TODO Tab and TabItem have to much dynamic behavior to snapshot test effectively
-describe.todo('Tabs Component', () => {
-	test.todo('renders Tabs component correctly', async ({ renderComponent }) => {
+describe('Tabs Component', () => {
+	test('renders Tabs component correctly', async ({ renderComponent }) => {
 		const tabsContent = [];
 		for (let i = 1; i <= 3; i++) {
 			tabsContent.push(
